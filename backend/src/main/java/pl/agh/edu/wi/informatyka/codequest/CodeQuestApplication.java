ee6cd36fc08cf1b0c644d3ee29226f254ad685be
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-06-27 22:15:38 +0200

    Remove @EnableWebMvc

--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/CodeQuestApplication.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/CodeQuestApplication.java
@@ -2,10 +2,8 @@ package pl.agh.edu.wi.informatyka.codequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class CodeQuestApplication {
 
     public static void main(String[] args) {
