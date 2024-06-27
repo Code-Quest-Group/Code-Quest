3897e44ea7952caf1ffa7a723b4dd839ad3bb010
Miłosz Junak <milekjunak@gmail.com>
2024-06-27 22:17:43 +0200

    feat: support for 3 problems

--- frontend/src/app/components/views/problem-page/problem-page.component.ts
+++ frontend/src/app/components/views/problem-page/problem-page.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SeparatorComponent } from "./subcomponents";
 import { Observable } from 'rxjs';
 import { FormsModule } from '@angular/forms';
-import { switchMap, delay } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+import { json } from 'stream/consumers';
 
 @Component({
   selector: 'app-problem-page',
   standalone: true,
   templateUrl: './problem-page.component.html',
   styleUrls: ['./problem-page.component.scss'],
-  imports: [SeparatorComponent, HttpClientModule, FormsModule] 
+  imports: [SeparatorComponent, HttpClientModule, FormsModule, CommonModule] 
 })
-export class ProblemPageComponent {
+export class ProblemPageComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
-  code: string = `class Problem:
-  def solve(self, a, b):
-      return a + b
-`;
+  code: string = '';
+  problemName: string = '';
+  problems: any[] = [];
+  currentProblem: any = {};
+
+  ngOnInit() {
+    this.http.get<any[]>('http://localhost:8080/problems').subscribe(problems => {
+      this.problems = problems;
+      this.setProblem(this.problems[0]);
+    });
+  }
+
+  setProblem(problem: any) {
+    this.currentProblem = problem;
+    this.code = problem.codeTemplate;
+    this.problemName = problem.name;
+  }
 
   fillResults() {
     const codeEditorValue = (document.getElementById('code-editor') as HTMLTextAreaElement).value;
     const payload = {
       sourceCode: codeEditorValue,
-      problemId: "1",
+      problemId: this.currentProblem.problemId,
       language: "PYTHON",
     };
 
@@ -39,9 +54,8 @@ export class ProblemPageComponent {
         const jsonResult = JSON.parse(result);
         const resultsSection = document.getElementById('results-section');
         
-
         if (resultsSection) {
-          resultsSection.innerText = `Time: ${jsonResult.time}, Description: ${jsonResult.status.description}`;
+          resultsSection.innerText = `Name: ${jsonResult.problem_id}, Time: ${jsonResult.time}, Description: ${jsonResult.status.description}`;
         }
       });
   }
