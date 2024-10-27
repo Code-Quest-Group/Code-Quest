bf726f4c271e409e035ba2a123397d5f4348acac
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-10-27 22:32:20 +0100

    Add basic CRUD for problems

--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/problem/ProblemsController.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/problem/ProblemsController.java
@@ -3,10 +3,12 @@ package pl.agh.edu.wi.informatyka.codequest.problem;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController()
 @RequestMapping("/problems")
@@ -20,11 +22,14 @@ public class ProblemsController {
 
     @GetMapping("/{problemId}")
     public Problem getProblems(@PathVariable @Parameter(example = "add-two-numbers") String problemId) {
-        return problemsService.getProblem(problemId);
+        return problemsService
+                .getProblem(problemId)
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Problem " + problemId + " not found"));
     }
 
     @GetMapping()
-    public List<Problem> getProblem() {
-        return problemsService.getProblems();
+    public List<Problem> getAllProblems() {
+        return problemsService.getAllProblems();
     }
 }
--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/problem/ProblemsService.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/problem/ProblemsService.java
@@ -1,88 +1,26 @@
 package pl.agh.edu.wi.informatyka.codequest.problem;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpStatus;
+import java.util.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import pl.agh.edu.wi.informatyka.codequest.sourcecode.Language;
 
 @Service
 public class ProblemsService {
 
-    private static final String addTwoCodeTemplate =
-            """
-            class Problem:
+    private final ProblemsRepository problemsRepository;
 
-                def solve(self, a: int, b: int):
-                    pass
-
-
-            """;
-
-    private static final String fibonacciCodeTemplate =
-            """
-            class Problem:
-
-                def solve(self, n: int):
-                    pass
-
-
-            """;
-
-    private static final String countDivisorsCodeTemplate =
-            """
-            class Problem:
-
-                def solve(self, n: int, k: int):
-                    pass
-
-
-            """;
-
-    // this is a database lmao
-    Map<String, Problem> problems = Map.of(
-            "add-two-numbers",
-            new Problem(
-                    "add-two-numbers",
-                    "Add two numbers",
-                    "Calculate the sum of two numbers",
-                    List.of(Language.PYTHON),
-                    "int int",
-                    addTwoCodeTemplate,
-                    "1\n6\n2\n2\n3\n7\n4\n2\n5\n3\n",
-                    "7\n4\n10\n6\n8\n"),
-            "nth-fibonacci-number",
-            new Problem(
-                    "nth-fibonacci-number",
-                    "Nth Fibonacci number",
-                    "Find nth fibonacci number.",
-                    List.of(Language.PYTHON),
-                    "int",
-                    fibonacciCodeTemplate,
-                    "0\n1\n2\n3\n4\n5\n10\n18\n",
-                    "0\n1\n1\n2\n3\n5\n55\n2584"),
-            "count-divisors",
-            new Problem(
-                    "count-divisors",
-                    "Count divisors",
-                    "Count how many numbers less then n are divisible by k",
-                    List.of(Language.PYTHON),
-                    "int int",
-                    countDivisorsCodeTemplate,
-                    "10\n2\n100\n25\n", // two test cases n,k   10, 2 and 100, 25
-                    "4\n3\n"));
-
-    public List<Problem> getProblems() {
-        return new ArrayList<>(problems.values());
+    public ProblemsService(ProblemsRepository problemsRepository) {
+        this.problemsRepository = problemsRepository;
     }
 
-    public Problem getProblem(String problemId) {
-        Problem problem = problems.get(problemId);
-        if (problem == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Problem " + problemId + " not found");
-        }
-        return problem;
+    public Optional<Problem> getProblem(String problemId) {
+        return this.problemsRepository.findById(problemId);
+    }
+
+    public List<Problem> getAllProblems() {
+        return this.problemsRepository.findAll();
+    }
+
+    public void deleteProblem(String problemId) {
+        this.problemsRepository.deleteById(problemId);
     }
 }
