3a0c0f518a3de4e097b6a347d6f0d739a3e69434
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-11-19 00:46:00 +0100

    Improve validation errors return format

--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package pl.agh.edu.wi.informatyka.codequest.exception;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -21,9 +26,22 @@ public class GlobalExceptionHandler {
                         .build());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleAllExceptions(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField, Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(GenericResponse.builder()
+                        .status(ResponseStatus.ERROR)
+                        .message("Validation Error")
+                        .data(errors)
+                        .build());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception ex) {
-
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(GenericResponse.builder()
                         .status(ResponseStatus.ERROR)
