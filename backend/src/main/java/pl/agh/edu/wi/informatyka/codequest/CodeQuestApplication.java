6ac21c8793c33d1d8edffdffefc04b30c0217ff8
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-11-23 18:42:51 +0100

    Fix jackson serialization issue

--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/CodeQuestApplication.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/CodeQuestApplication.java
@@ -1,12 +1,23 @@
 package pl.agh.edu.wi.informatyka.codequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @SpringBootApplication
 public class CodeQuestApplication {
+
+    public CodeQuestApplication(RequestMappingHandlerAdapter handlerAdapter) {
+        this.handlerAdapter = handlerAdapter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CodeQuestApplication.class, args);
@@ -15,5 +26,19 @@ public class CodeQuestApplication {
     @PostConstruct
     void started() {
         TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC"));
+    }
+
+    private final RequestMappingHandlerAdapter handlerAdapter;
+
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        handlerAdapter
+                .getMessageConverters()
+                .forEach(c -> {
+                    if (c instanceof MappingJackson2HttpMessageConverter jsonMessageConverter) {
+                        ObjectMapper objectMapper = jsonMessageConverter.getObjectMapper();
+                        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+                    }
+                });
     }
 }
