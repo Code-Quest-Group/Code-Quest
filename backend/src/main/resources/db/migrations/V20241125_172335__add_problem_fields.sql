76636fa1a7e2c8e7386cb6cc23032b8935fdc6eb
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-11-25 17:58:26 +0100

    Add fields to Problem

---
+++ backend/src/main/resources/db/migrations/V20241125_172335__add_problem_fields.sql
@@ -0,0 +1,6 @@
+ALTER TABLE problems
+    ADD hints                       TEXT        NULL,
+    ADD rating                      DOUBLE      NULL,
+    ADD tags                        TEXT        NULL,
+    ADD example_testcases           TEXT        NULL,
+    ADD example_expected_result     TEXT        NULL;
