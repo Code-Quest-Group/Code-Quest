8a865e41044fd22f2a55ae6edc335728fdf81c4d
Mikolaj Maslak <nicholasdev7@gmail.com>
2024-11-25 19:36:58 +0100

    Add favicon

--- backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/config/WebConfig.java
+++ backend/src/main/java/pl/agh/edu/wi/informatyka/codequest/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // Allow all origins
                 .allowedMethods("*") // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
                 .allowedHeaders("*"); // Allow all headers
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
     }
 
     @Bean
